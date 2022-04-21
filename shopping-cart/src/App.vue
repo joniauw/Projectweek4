4195253d44d38a256c4812d44ce3db4b9e0daf48
Sandeep Singh <sandeep159@outlook.com>
2022-04-21 10:05:29 +0200

    Front-end VueJS shopping cart added

---
+++ shopping-cart/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <nav>
+      <NavHeader />
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted(){
+    this.$store.dispatch("getProducts");
+  }
+}
+</script>
+
+<script>
+import NavHeader from "@/NavHeader.vue"
+export default {
+  components: {
+    NavHeader
+  },
+  mounted(){
+    this.$store.dispatch("getProducts");
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ shopping-cart/src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Login from '../Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    }
+  },
+  {
+    path: '/loginwithtoken', name: 'loginwithtoken', component: Login
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
