01473e69408867393f1e68969f5162c28917b200
Sandeep Singh <sandeep159@outlook.com>
2022-04-21 11:07:28 +0200

    Dockerfile aangepast

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# build stage
+FROM node:lts-alpine as build-stage
+WORKDIR /shopping-cart
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+# production stage
+FROM nginx:1.17
+COPY ./shopping-cart/nginx.conf /etc/nginx/nginx.conf
+WORKDIR /Projectweek4
+COPY --from=build-stage /shopping-cart/dist .
+EXPOSE 8080:8080
+CMD ["nginx", "-g", "daemon off;"]
--- dist/js/about-legacy.44ac9ff9.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkshopping_cart"]=self["webpackChunkshopping_cart"]||[]).push([[443],{830:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],a=n(1),r={},c=(0,a.Z)(r,s,u,!1,null,null,null),l=c.exports}}]);
-//# sourceMappingURL=about-legacy.44ac9ff9.js.map
